1c05c6bc8087f18b91aad219add2311a541ae0aa
tql247 <toitenlalinh9xpro@gmail.com>
2020-08-04 08:36:01 +0700

    add

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const path = require("path");
+var app = express();
+var request = require('request');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/Busmap.html'));
+});
+
+router.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/about.html'))
+})
+
+router.post("/getRoute", (req, res) => {
+  var options = {
+    'method': 'GET',
+    'url': `http://apicms.ebms.vn/prediction/predictbystopid/${req.body.id}`,
+    'headers': {
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.send(response.body);
+  });
+})
+
+app.use("/", router);
+app.use("/assets", express.static(__dirname + "/views/assets"))
+app.listen(process.env.port || 3001);
+
+console.log("Running at Port 3001");
+
+
+module.exports = app;
