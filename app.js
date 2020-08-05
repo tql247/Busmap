5ec93465e2a2d5b72947ad3d44c734cfe0ca8de3
tql247 <toitenlalinh9xpro@gmail.com>
2020-08-05 17:03:01 +0700

    dasdas

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var express = require('express');
 const path = require("path");
 var app = express();
 var request = require('request');
-const PORT = 5000
-// const PORT = process.env.PORT || 5000
+// const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 
 
